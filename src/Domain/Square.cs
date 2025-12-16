73e56b85b24a180a1459880cb525ccc8ee79fb0d
or1on <105861555+MariuszJanowiak@users.noreply.github.com>
2025-12-16 20:38:41 +0100

    Add square record

---
+++ src/Domain/Square.cs
@@ -0,0 +1,27 @@
+﻿public sealed record Square
+{
+    public char File { get;  }
+
+    public int Rank { get; }
+
+    private Square(char file, int rank)
+    {
+        File = file;
+        Rank = rank;
+    }
+
+    // Controlled Creation
+
+    public static bool TryFrom(char file, int rank, out Square? square)
+    {
+        file = char.ToLowerInvariant(file);
+
+        if (rank is < 1 or > 8 || file < 'a' || file > 'h')
+        {
+            square = null;
+            return false;
+        }
+        square = new Square(file, rank);
+        return true;
+    }
+}
