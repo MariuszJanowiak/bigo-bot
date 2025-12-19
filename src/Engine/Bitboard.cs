c184bfbf27b8385421ca65fb487b6c3bf1f2c075
or1on <105861555+MariuszJanowiak@users.noreply.github.com>
2025-12-19 14:54:15 +0100

    Add Basic Bitboard class

---
+++ src/Engine/Bitboard.cs
@@ -0,0 +1,12 @@
+﻿using Domain.ValueObjects;
+
+namespace Engine
+{
+    public static class Bitboard
+    {
+        public static ulong Mask(Square square)
+        {
+            return 1UL << SquareIndexMapper.Map(square);
+        }
+    }
+}
--- src/Engine/SquareIndexMapper.cs
+++ src/Engine/SquareIndexMapper.cs
@@ -14,4 +14,16 @@ public static class SquareIndexMapper
 
         return (rankIndex * 8) + fileIndex;
     }
+
+    public static bool TryMap(int index, out Square? square)
+    {
+        if (index is < 0 or > 63)
+        {
+            square = null;
+            return false;
+        }
+        int rank = (index / 8) + 1;
+        char file = (char)('a' + (index % 8));
+        return Square.TryFrom(file, rank, out square);
+    }
 }
