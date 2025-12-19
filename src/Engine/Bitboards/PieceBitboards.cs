e97114dafd08e78c3d53f05576f49fdfa22a3784
or1on <105861555+MariuszJanowiak@users.noreply.github.com>
2025-12-19 17:02:44 +0100

    Add Pawn move engine + Clean PieceBitBoards class code

--- src/Engine/Bitboards/PieceBitboards.cs
+++ src/Engine/Bitboards/PieceBitboards.cs
@@ -1,87 +1,69 @@
-﻿using Domain.ValueObjects;
-using System.Text;
+﻿using System.Text;
+using Domain.ValueObjects;
 
-namespace Engine.Bitboards
+namespace Engine.Bitboards;
+
+public sealed class PieceBitboards
 {
-    public class PieceBitboards
+    public ulong WhitePawns { get; private set; }
+    public ulong BlackPawns { get; private set; }
+
+    public void AddWhitePawn(Square square) => WhitePawns |= Bitboard.Mask(square);
+    public void AddBlackPawn(Square square) => BlackPawns |= Bitboard.Mask(square);
+
+    public void RemoveWhitePawn(Square square) => WhitePawns &= ~Bitboard.Mask(square);
+    public void RemoveBlackPawn(Square square) => BlackPawns &= ~Bitboard.Mask(square);
+
+    public bool IsOccupied(Square square)
     {
-        public ulong WhitePawns { get; private set; }
-        public ulong BlackPawns { get; private set; }
+        ulong mask = Bitboard.Mask(square);
+        return (WhitePawns & mask) != 0 || (BlackPawns & mask) != 0;
+    }
 
-        public void AddWhitePawn(Square square)
+    public IEnumerable<string> Ascii(Square? highlight = null)
+    {
+        for (int rank = 8; rank > 0; rank--)
         {
-            WhitePawns |= Bitboard.Mask(square);
-        }
+            var line = new StringBuilder(8 * 3);
 
-        public void AddBlackPawn(Square square)
-        {
-            BlackPawns |= Bitboard.Mask(square);
-        }
-
-        public void RemoveWhitePawn(Square square)
-        {
-            WhitePawns &= ~Bitboard.Mask(square);
-        }
-
-        public void RemoveBlackPawn(Square square)
-        {
-            BlackPawns &= ~Bitboard.Mask(square);
-        }
-
-        public bool IsOccupied(Square square)
-        {
-            ulong mask = Bitboard.Mask(square);
-            return (WhitePawns & mask) is not 0 || (BlackPawns & mask) is not 0;
-        }
-
-        public IEnumerable<string> Ascii(Square? highlight = null)
-        {
-            for (int rank = 8; rank > 0; rank--)
+            for (char file = 'a'; file <= 'h'; file++)
             {
-                var line = new StringBuilder(8 * 3); // Each square takes up to 3 
+                if (!Square.TryFrom(file, rank, out var square) || square is null)
+                    throw new InvalidOperationException();
 
-                for (char file = 'a'; file <= 'h'; file++)
-                {
-                    if (!Square.TryFrom(file, rank, out var currentSquare) || currentSquare is null)
-                        throw new InvalidOperationException();
+                ulong mask = Bitboard.Mask(square);
+                bool isHighlight = highlight is not null && square == highlight;
 
-                    ulong mask = Bitboard.Mask(currentSquare);
-                    bool isHighlight = highlight is not null && currentSquare == highlight;
+                line.Append(isHighlight ? '[' : ' ');
 
-                    line.Append(isHighlight ? '[' : ' ');
+                if ((WhitePawns & mask) != 0) line.Append('P');
+                else if ((BlackPawns & mask) != 0) line.Append('p');
+                else line.Append('.');
 
-                    if ((WhitePawns & mask) != 0)
-                        line.Append('P');
-                    else if ((BlackPawns & mask) != 0)
-                        line.Append('p');
-                    else
-                        line.Append('.');
-
-                    line.Append(isHighlight ? ']' : ' ');
-                }
-
-                yield return line.ToString();
-            }
-        }
-
-        public string AsciiCoordinates(Square? highlight = null)
-        {
-            var sb = new StringBuilder();
-
-            int rank = 8;
-            foreach (var line in Ascii(highlight))
-            {
-                sb.Append(rank);
-                sb.Append(" |");
-                sb.Append(line);
-                sb.AppendLine("|");
-                rank--;
+                line.Append(isHighlight ? ']' : ' ');
             }
 
-            sb.Append("    ");
-            sb.AppendLine("a  b  c  d  e  f  g  h");
-
-            return sb.ToString();
+            yield return line.ToString();
         }
+    }
+
+    public string AsciiCoordinates(Square? highlight = null)
+    {
+        var sb = new StringBuilder();
+
+        int rank = 8;
+        foreach (var line in Ascii(highlight))
+        {
+            sb.Append(rank);
+            sb.Append(" |");
+            sb.Append(line);
+            sb.AppendLine("|");
+            rank--;
+        }
+
+        sb.Append("    ");
+        sb.AppendLine("a  b  c  d  e  f  g  h");
+
+        return sb.ToString();
     }
 }
---
+++ src/Engine/Moves/PawnMoves.cs
@@ -0,0 +1,37 @@
+﻿using Domain.ValueObjects;
+using Engine.Bitboards;
+
+namespace Engine.Moves;
+
+public static class PawnMoves
+{
+    public static ulong WhiteOneStepTargets(PieceBitboards board)
+    {
+        ulong occupied = board.WhitePawns | board.BlackPawns;
+        return (board.WhitePawns << 8) & ~occupied;
+    }
+
+    public static ulong BlackOneStepTargets(PieceBitboards board)
+    {
+        ulong occupied = board.WhitePawns | board.BlackPawns;
+        return (board.BlackPawns >> 8) & ~occupied;
+    }
+
+    public static IEnumerable<(Square From, Square To)> WhiteOneStepMoves(PieceBitboards board)
+    {
+        ulong targets = WhiteOneStepTargets(board);
+
+        for (int toIndex = 0; toIndex < 64; toIndex++)
+        {
+            ulong toMask = 1UL << toIndex;
+            if ((targets & toMask) == 0) continue;
+
+            int fromIndex = toIndex - 8;
+
+            if (!SquareIndexMapper.TryMap(fromIndex, out var from) || from is null) continue;
+            if (!SquareIndexMapper.TryMap(toIndex, out var to) || to is null) continue;
+
+            yield return (from, to);
+        }
+    }
+}
